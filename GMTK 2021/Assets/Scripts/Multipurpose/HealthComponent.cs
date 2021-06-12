ca94063d2df2a44ecd46c9afc99986303afe66aa
lewisjet <lewisjet11@outlook.com>
2021-06-12 09:05:46 +0100

    2nd try

--- GMTK 2021/Assets/Scripts/Multipurpose/HealthComponent.cs
+++ GMTK 2021/Assets/Scripts/Multipurpose/HealthComponent.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthComponent : MonoBehaviour
+public class HealthComponent : MonoBehaviour, IGuidRegistered
 {
+    public string Guid {get; set;}
     [SerializeField] private float _health;
     [Tooltip("Leave at 0 for no maximum")]
     [SerializeField] private float _maxHealth;
@@ -14,4 +15,9 @@ public class HealthComponent : MonoBehaviour
          if(_health > _maxHealth && _maxHealth != 0) { _health = _maxHealth; }
          if(_health <= 0){ Destroy(gameObject); }
      }}
+
+}
+public interface IGuidRegistered
+{
+     string Guid { get; set;}
 }
