80051b823ff7daa013a6907ab88e53933d47d88b
EmpowerAnimation <empoweranimations@gmail.com>
2021-06-13 18:58:55 +0900

    Main Menu + Game Over Screen.

--- GMTK 2021/Assets/Scripts/Menus/MainMenu.cs
+++ GMTK 2021/Assets/Scripts/Menus/MainMenu.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
 
     public GameObject[] Buttons;
+    public Vector2[] ButtonPos;
+    public GameObject ButtonCursor;
     public int SelectedButton;
 
     // Start is called before the first frame update
@@ -17,9 +20,19 @@ public class MainMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown("w")) SelectedButton++;
-        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown("s")) SelectedButton--;
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown("s")) SelectedButton++;
+        else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown("w")) SelectedButton--;
         if (SelectedButton > 3) SelectedButton = 3;
         else if (SelectedButton < 0) SelectedButton = 0;
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            if (SelectedButton == 0) SceneManager.LoadScene(3);
+            else if (SelectedButton == 1) SceneManager.LoadScene(1);
+            else if (SelectedButton == 2) SceneManager.LoadScene(2);
+            else if (SelectedButton == 3) Application.Quit();
+        }
+
+        ButtonCursor.transform.position = ButtonPos[SelectedButton];
     }
 }
